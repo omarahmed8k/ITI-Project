799c6d834e627ea39023981da26c95c4a4c05890
mohamed nader <70781182+mohamednader973@users.noreply.github.com>
2021-09-17 02:08:10 +0200

    file system modification
    file system modification

--- app/Http/Controllers/Profile/EditController.php
+++ app/Http/Controllers/Profile/EditController.php
@@ -8,6 +8,7 @@ use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\File;
 class EditController extends Controller
 {
     public function __construct()
@@ -32,12 +33,34 @@ class EditController extends Controller
       $request->validate([
           'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
       ]);
+      $user = Auth::user();
+      $userId=$user->id;
 
        $imageName = time().'.'.$request->image->extension();
 
-      $request->image->move(public_path('img'), $imageName);
-      $user = Auth::user();
-      $userId=$user->id;
+
+// code of creating user folders
+      $folderName='user'.$userId;
+      //File::makeDirectory("storage\app\public");
+      $path = public_path($folderName);
+
+    if(!File::isDirectory($path)){
+
+        File::makeDirectory($path, 0777, true, true);
+
+    }
+// code of creating user folders
+
+
+// code of creating user folders
+    $request->image->move(public_path($folderName), $imageName);
+      //$request->file->store($folderName, 'public');
+      // $product = new User([
+      //           "name" => $request->get('name'),
+      //           "file_path" => $request->image->hashName()
+      //       ]);
+      //       $product->save();
+
       User::where('id',$userId)->update([
       'name'=>$request->nameedit,
       'password'=>Hash::make($request->passedit),
