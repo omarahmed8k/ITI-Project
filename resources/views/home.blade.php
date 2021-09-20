316ae363d41cd00390ddb58c330e11d6bffb2a60
omarahmed8k <omarahmed8k@gmail.com>
2021-09-20 10:15:54 +0200

    Edit Design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,123 +1,115 @@
 @extends('layouts.navbar')
 @section('content')
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
 
 
-                @isset($posts)
-                    @foreach ($posts as $item)
-                    @foreach($followers as $item5)
-                        @if( ($item->user_id == $item5->user_id && $item5->follower_id == auth()->user()->id) || ($item->user_id == auth()->user()->id))
+            @isset($posts)
+            @foreach ($posts as $item)
+            @foreach($followers as $item5)
+            @if( ($item->user_id == $item5->user_id && $item5->follower_id == auth()->user()->id) || ($item->user_id == auth()->user()->id))
 
 
-                        <div class="homePost d-flex flex-column">
-                            <div class="postOwner">
-                                <a href="{{ url('/profile'.'/'.$item->user->id) }}"><img
-                                        src="{{ asset( $item->user->avatar ) }}"><span> {{ $item->user->name }}</span></a>
-                            </div>
-                            <div class="postImg">
-                                <img class="img-fluid" src="{{ asset('uploads/post/'.$item->image) }}">
-                            </div>
-                            <div class="postFeatures">
-                                <ul>
-                                    <li>
-                                    <form  action="{{ url('add-likes') }}" method="POST" enctype="multipart/form-data">
-                                    @csrf
-                                    <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
-                                    <input type="hidden" name="postID" value="{{ $item->id }}">
+            <div class="homePost d-flex flex-column">
+                <div class="postOwner">
+                    <a href="{{ url('/profile'.'/'.$item->user->id) }}"><img src="{{ asset( $item->user->avatar ) }}"><span> {{ $item->user->name }}</span></a>
+                </div>
+                <div class="postImg">
+                    <img class="img-fluid" src="{{ asset('uploads/post/'.$item->image) }}">
+                </div>
+                <div class="postFeatures">
+                    <ul>
+                        <li>
+                            <form action="{{ url('add-likes') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
+                                <input type="hidden" name="postID" value="{{ $item->id }}">
 
-                                    <button class="postLike" type="submit" ><i class="far fa-heart"></i></button>
+                                <button class="postLike" type="submit"><i class="far fa-heart"></i></button>
+                            </form>
+                        </li>
 
-                                    </form>
-                                    </li>
+                        <li><button class="postLike" onclick="window.location.href='/post/{{  $item->id }}';"><i class="far fa-comment"></i></button></li>
+                    </ul>
+                    <ul class="ml-auto">
+                        <li>
+                            <form action="{{ url('save-post') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
+                                <input type="hidden" name="postID" value="{{ $item->id }}">
 
-                                    <li><button onclick="window.location.href='/post/{{  $item->id }}';" ><i class="far fa-comment"></i></button></li>
-                                </ul>
-                                <ul class="ml-auto">
-                                    <li>
-                                        <form  action="{{ url('save-post') }}" method="POST" enctype="multipart/form-data">
-                                            @csrf
-                                            <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
-                                            <input type="hidden" name="postID" value="{{ $item->id }}">
+                                <button class="postLike" type="submit"><i class="far fa-bookmark"></i></button>
 
-                                            <button class="postLike" type="submit" ><i class="far fa-bookmark"></i></button>
+                            </form>
+                        </li>
 
-                                        </form>
-                                    </li>
+                    </ul>
+                    <div class="postLikes">
+                        @php
+                        $counter=0;
+                        foreach ($likes as $item2){
+                        if($item2->post_id == $item->id)
+                        {
 
-                                </ul>
-                                <div class="postLikes">
-                                @php
-                                $counter=0;
-                                foreach ($likes as $item2){
-                                    if($item2->post_id == $item->id)
-                                    {
-
-                                        $counter=$counter+1;
-                                    }//end if
+                        $counter=$counter+1;
+                        }//end if
 
 
-                                }//end foreach
-                                    echo "$counter";
-                                    echo" likes";
-                                echo"</div>";
-                                @endphp
-                                <div class="postCaption">
-                                    <a class="font-weight-bold d-block w-100"
-                                        href="{{url('/profile')}}"> {{ $item->user_name }} </a>
-                                    <p class="m-0">
-                                        @php
+                        }//end foreach
+                        echo "$counter";
+                        echo" likes";
+                        echo"</div>";
+                    @endphp
+                    <div class="postCaption">
+                        <a class="font-weight-bold d-block w-100" href="{{url('/profile')}}"> {{ $item->user_name }} </a>
+                        <p class="m-0">
+                            @php
 
-                                            $cap = $item->caption;
-                                            foreach($tags as $tag){
+                            $cap = $item->caption;
+                            foreach($tags as $tag){
 
-                                                preg_match_all('/#(\w+)/', $cap, $matches);
-                                                $ncap = str_replace($tag->name,"<a href='/tags/$tag->id '>$tag->name</a>",$cap);
-                                                $cap = $ncap;
+                            preg_match_all('/#(\w+)/', $cap, $matches);
+                            $ncap = str_replace($tag->name,"<a href='/tags/$tag->id '>$tag->name</a>",$cap);
+                            $cap = $ncap;
 
-                                            }
-                                            echo"$cap";
-                                        @endphp
-                                    </p>
-                                </div>
-                            </div>
-                            <span class="text-muted px-2">Comments</span>
-                            @foreach ($comments as $item1)
-                                @if ($item->id == $item1->post_id)
-                                    <div class="postComments">
-                                        <a class="pl-3 pr-1 font-weight-bold"
-                                            href="{{ url('/profile'.'/'.$item1->user->id) }}"> {{ $item1->user->name }} </a>
-                                        <p class="pl-3 pr-1 pb-2 m-0">
-                                            {{$item1->comment}}
-                                        </p>
-                                    </div>
-                                @endif
-                            @endforeach
+                            }
+                            echo"$cap";
+                            @endphp
+                        </p>
+                    </div>
+                </div>
+                <span class="text-muted px-2">Comments</span>
+                @foreach ($comments as $item1)
+                @if ($item->id == $item1->post_id)
+                <div class="postComments">
+                    <a class="pl-3 pr-1 font-weight-bold" href="{{ url('/profile'.'/'.$item1->user->id) }}"> {{ $item1->user->name }} </a>
+                    <p class="pl-3 pr-1 pb-2 m-0">
+                        {{$item1->comment}}
+                    </p>
+                </div>
+                @endif
+                @endforeach
 
-                            <div class="postAddComment">
-                                    <form  action="{{ url('add-comments') }}" method="POST" enctype="multipart/form-data">
-                                    @csrf
-                                    <textarea placeholder="Comment" name="comment" ></textarea>
-                                    <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
-                                    <input type="hidden" name="postID" value="{{ $item->id }}">
-
-                                    <button class="btn btn-primary add-comment" type="submit" >Post</button>
-
-                                    </form>
-                            </div>
-                        </div>
-                        @break
-                        @endif
-
-
-                        @endforeach
-                    @endforeach
-                @endisset
-
+                <form class="postAddComment" action="{{ url('add-comments') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <textarea placeholder="Comment" name="comment"></textarea>
+                    <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
+                    <input type="hidden" name="postID" value="{{ $item->id }}">
+                    <button class="btn" type="submit">Post</button>
+                </form>
             </div>
+            @break
+            @endif
+
+
+            @endforeach
+            @endforeach
+            @endisset
+
         </div>
     </div>
+</div>
 
 @endsection
--- resources/views/post/viewPost.blade.php
+++ resources/views/post/viewPost.blade.php
@@ -8,7 +8,7 @@
             <div class="postPage">
                 <div class="img">
 
-                @foreach ($posts as $item)
+                    @foreach ($posts as $item)
                     @if($item->id == $ids)
                     <img class="img-fluid" src="{{ asset('uploads/post/'.$item->image)}}" alt="">
 
@@ -18,39 +18,39 @@
                     <a class="owner" href="{{  url('/profile'.'/'.$item->user->id)}}"><img src="{{asset('img/acpc.jpeg')}}"><span>{{$item->user->name}}</span></a>
 
                     <div class="comments-container">
-                         <div class="caption">
+                        <div class="caption">
                             <a href="{{  url('/profile'.'/'.$item->user->id) }}"><img class="img-fluid" src="{{asset('img/acpc.jpeg')}}"><span>{{$item->user->name}} </span></a>
                             <p class="m-0">
-                                <span class="text-muted"> 
+                                <span class="text-muted">
                                     @php
 
-                                            $cap = $item->caption;
-                                            foreach($tags as $tag){
+                                    $cap = $item->caption;
+                                    foreach($tags as $tag){
 
-                                                preg_match_all('/#(\w+)/', $cap, $matches);
-                                                $ncap = str_replace($tag->name,"<a href='/tags/$tag->id '>$tag->name</a>",$cap);
-                                                $cap = $ncap;
+                                    preg_match_all('/#(\w+)/', $cap, $matches);
+                                    $ncap = str_replace($tag->name,"<a href='/tags/$tag->id '>$tag->name</a>",$cap);
+                                    $cap = $ncap;
 
-                                            }
-                                            echo"$cap";
-                                        @endphp
-                                    
-                                    
-                                     </span>
+                                    }
+                                    echo"$cap";
+                                    @endphp
+
+
+                                </span>
                             </p>
                             <span class="time">{{$item->created_at}}</span>
                         </div>
                         @foreach ($comments as $item1)
-                                @if ($ids == $item1->post_id)
-                                        <div class="comment">
-                                <a href="{{  url('/profile'.'/'.$item1->user->id)}}"><img class="img-fluid" src="{{asset('img/acpc.jpeg')}}"><span> {{ $item1->user->name }} </span></a>
-                                <p class="m-0">
-                                    <span class="text-muted">  {{$item1->comment}} </span>
-                                </p>
-                                <span class="time">{{$item1->created_at}}</span>
-                            </div>
-                                @endif
-                            @endforeach
+                        @if ($ids == $item1->post_id)
+                        <div class="comment">
+                            <a href="{{  url('/profile'.'/'.$item1->user->id)}}"><img class="img-fluid" src="{{asset('img/acpc.jpeg')}}"><span> {{ $item1->user->name }} </span></a>
+                            <p class="m-0">
+                                <span class="text-muted"> {{$item1->comment}} </span>
+                            </p>
+                            <span class="time">{{$item1->created_at}}</span>
+                        </div>
+                        @endif
+                        @endforeach
 
 
 
@@ -58,59 +58,48 @@
                     <div class="features">
                         <ul>
                             <li>
-                                <form  action="{{ url('add-likes2') }}" method="POST" enctype="multipart/form-data">
+                                <form action="{{ url('add-likes2') }}" method="POST" enctype="multipart/form-data">
                                     @csrf
                                     <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
                                     <input type="hidden" name="postID" value="{{ $ids }}">
-
-                                    <button class="postLike" type="submit" ><i class="far fa-heart"></i></button>
-
-                                 </form>
-
+                                    <button class="postLike" type="submit"><i class="far fa-heart"></i></button>
+                                </form>
                             </li>
                             <li>
-
-                                     <form  action="{{ url('save-post2') }}" method="POST" enctype="multipart/form-data">
-                                            @csrf
-                                            <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
-                                            <input type="hidden" name="postID" value="{{ $ids }}">
-
-                                            <button class="postLike" type="submit" ><i class="far fa-bookmark"></i></button>
-
-                                        </form>
+                                <form action="{{ url('save-post2') }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
+                                    <input type="hidden" name="postID" value="{{ $ids }}">
+                                    <button class="postLike" type="submit"><i class="far fa-bookmark"></i></button>
+                                </form>
                             </li>
                         </ul>
                         <div class="likes">
-                           @php
-                                $counter=0;
-                                foreach ($likes as $item2){
-                                    if($item2->post_id == $ids)
-                                    {
+                            @php
+                            $counter=0;
+                            foreach ($likes as $item2){
+                            if($item2->post_id == $ids)
+                            {
+                            $counter=$counter+1;
+                            }//end if
 
-                                        $counter=$counter+1;
-                                    }//end if
 
-
-                                }//end foreach
-                                    echo "$counter";
-                                    echo" likes";
-                                echo"</div>";
-                                @endphp
+                            }//end foreach
+                            echo "$counter";
+                            echo" likes";
+                            echo"</div>";
+                        @endphp
                     </div>
-                    <div class="add-comment">
-                        <form  action="{{ url('add-comments2') }}" method="POST" enctype="multipart/form-data">
-                                    @csrf
-                                    <textarea placeholder="Comment" name="comment" ></textarea>
-                                    <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
-                                    <input type="hidden" name="postID" value="{{ $ids }}">
-
-                                    <button class="btn btn-primary add-comment" type="submit" >Post</button>
-
-                                </form>
-                        </div>
+                    <form class="w-100 add-comment" action="{{ url('add-comments2') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <textarea placeholder="Comment" name="comment"></textarea>
+                        <input type="hidden" name="userID" value="{{ auth()->user()->id }}">
+                        <input type="hidden" name="postID" value="{{ $ids }}">
+                        <button class="btn" type="submit">Post</button>
+                    </form>
                 </div>
                 @endif
-                 @endforeach
+                @endforeach
             </div>
         </div>
     </div>
