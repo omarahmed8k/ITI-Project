66ba97bca9190a19c5551a982e041eddc99baf38
mohamed nader <70781182+mohamednader973@users.noreply.github.com>
2021-09-20 02:14:51 +0200

    edit search
    edit search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -8,10 +8,12 @@
     $profilePath='/profile/'.$foundusers[$i]->id;
     echo $profilePath;
     @endphp
-    <tr onclick="location.href={{$profilePath}}" style="cursor:pointer; border-bottom-style: solid; border-bottom-color: #f8fafc;">
-      <td><img src="{{$path}}" alt="img" style="width:6rem; height:3rem" class="rounded-circle"></td>
-      <td>{{$foundusers[$i]->username}}</td>
-    </tr>
+    {{-- onclick="location.href='http://google.com'" --}}
+      <tr  onclick="location.href='{{$profilePath}}'" style="cursor:pointer; border-bottom-style: solid; border-bottom-color: #f8fafc;">
+          <td><img src="{{$path}}" alt="img" style="width:6rem; height:3rem" class="rounded-circle"></td>
+          <td>{{$foundusers[$i]->username}}</td>
+      </tr>
+
   @endfor
 
 </table>
