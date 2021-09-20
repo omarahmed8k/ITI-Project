40d0a7e4ff317ae8dd8e359bc14fde2898c207b9
HebaAbdElhamed <90287916+HebaAbdElhamed@users.noreply.github.com>
2021-09-20 03:06:55 +0200

    Replace user name with name

--- resources/views/post/viewPost.blade.php
+++ resources/views/post/viewPost.blade.php
@@ -4,22 +4,22 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-        
+
             <div class="postPage">
                 <div class="img">
-                
+
                 @foreach ($posts as $item)
                     @if($item->id == $ids)
                     <img class="img-fluid" src="{{ asset('uploads/post/'.$item->image)}}" alt="">
-                   
-                    
+
+
                 </div>
                 <div class="details">
-                    <a class="owner" href="{{ url('/profile') }}"><img src="{{asset('img/acpc.jpeg')}}"><span>{{$item->user->username}}</span></a>
-                   
+                    <a class="owner" href="{{ url('/profile') }}"><img src="{{asset('img/acpc.jpeg')}}"><span>{{$item->user->name}}</span></a>
+
                     <div class="comments-container">
                          <div class="caption">
-                            <a href="{{ url('/profile') }}"><img class="img-fluid" src="{{asset('img/acpc.jpeg')}}"><span>{{$item->user->username}} </span></a>
+                            <a href="{{ url('/profile') }}"><img class="img-fluid" src="{{asset('img/acpc.jpeg')}}"><span>{{$item->user->name}} </span></a>
                             <p class="m-0">
                                 <span class="text-muted"> {{ $item->caption }} </span>
                             </p>
@@ -28,17 +28,17 @@
                         @foreach ($comments as $item1)
                                 @if ($ids == $item1->post_id)
                                         <div class="comment">
-                                <a href="{{ url('/profile') }}"><img class="img-fluid" src="{{asset('img/acpc.jpeg')}}"><span> {{ $item1->user->username }} </span></a>
+                                <a href="{{ url('/profile') }}"><img class="img-fluid" src="{{asset('img/acpc.jpeg')}}"><span> {{ $item1->user->name }} </span></a>
                                 <p class="m-0">
                                     <span class="text-muted">  {{$item1->comment}} </span>
                                 </p>
                                 <span class="time">{{$item1->created_at}}</span>
                             </div>
                                 @endif
-                            @endforeach  
-                        
-                       
-                        
+                            @endforeach
+
+
+
                     </div>
                     <div class="features">
                         <ul>
@@ -51,7 +51,7 @@
                                     <button class="postLike" type="submit" ><i class="far fa-heart"></i></button>
 
                                  </form>
-                            
+
                             </li>
                             <li>
 
@@ -71,10 +71,10 @@
                                 foreach ($likes as $item2){
                                     if($item2->post_id == $ids)
                                     {
-                                
+
                                         $counter=$counter+1;
                                     }//end if
-                                    
+
 
                                 }//end foreach
                                     echo "$counter";
@@ -93,7 +93,7 @@
 
                                 </form>
                         </div>
-                </div> 
+                </div>
                 @endif
                  @endforeach
             </div>
